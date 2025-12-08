e75b16ffc3639959b0da07f06db2b98110a3469e
jeel-work-mob-06 <jeelparekh2024@gmail.com>
2025-12-08 11:29:19 +0530

    created fragment and just give refrence in main activity_main.xml

--- app/src/main/java/com/activityandfragment/MainActivity.java
+++ app/src/main/java/com/activityandfragment/MainActivity.java
@@ -1,14 +1,19 @@
 package com.activityandfragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button openFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +21,15 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+    openFragment = findViewById(R.id.open_fragment);
 
+    openFragment.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.activity_layout,new DemoFragment()).commit();
+        }
+    });
 
     }
 }
